5ff7df38f004648ecb2acac99173833eabda24ea
Ruko97 <ruko97@gmail.com>
2018-10-01 12:53:46 +0600

    Add files via upload

---
+++ 8B6T/src/BT/Sender.java
@@ -0,0 +1,16 @@
+package BT;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Sender {
+    private static Map<String, String> map = new HashMap<String, String>();
+
+    public static void createMap() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

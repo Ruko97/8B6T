ab534629840ae0b7b7669c784febfdfb43e8cd33
Ruko97 <ruko97@gmail.com>
2018-10-01 12:57:23 +0600

    Update Sender.java

--- 8B6T/src/BT/Sender.java
+++ 8B6T/src/BT/Sender.java
@@ -1,13 +1,25 @@
 package BT;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 class Sender {
-    private static Map<String, String> map = new HashMap<String, String>();
+    private static Map<Integer, String> map = new HashMap<Integer, String>();
 
-    public static void createMap() {
-
+    public static void createMap() throws IOException {
+        FileReader fileReader = new FileReader("map.txt");
+        Scanner
+        
+        for(int i=0; i<256; i++) {
+            String input = 
+            map.put(i, )
+        }
+        
+        fileReader.close();
     }
 
     public static void main(String[] args) {
